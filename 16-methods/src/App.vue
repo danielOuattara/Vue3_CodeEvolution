db3a180f81d45bb727a1d584598f3a265ad57fb0
danielOuattara <daniel.ouattara@gmx.com>
2022-12-29 23:08:57 +0100

    methods

---
+++ 16-methods/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <p>Adder method output: {{ adder() }}</p>
+  <p>Adder2 method output: {{ adder2(2, 5, 9) }}</p>
+  <p>multiplicator method output: {{ multiplicator(12) }}</p>
+  <p>multiplicator method output: {{ multiplicator(9) }}</p>
+  <p>multiplicator method output: {{ multiplicator(five) }}</p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+
+  data() {
+    return {
+      multiplier: 3,
+      five: 5,
+    }
+  },
+
+  methods: {
+    adder() {
+      return 2 + 3 + 4
+    },
+    adder2(a, b, c) {
+      return a + b + c
+    },
+
+    multiplicator(number) {
+      return this.multiplier * number
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
