14262106b5750926797f48e147bc5543053aacd0
danielOuattara <daniel.ouattara@gmx.com>
2022-12-29 23:09:12 +0100

    event handling

---
+++ 17-event-handling-part-1/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <h2>{{ name }}</h2>
+  <p>
+    <button v-on:click="changeName">click to change Name</button>
+  </p>
+  <p>
+    <button v-on:mouseover="this.name = 'Daniel'">
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button v-on:mouseover="name = 'Daniel'">
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button
+      v-on:mouseover="
+        this.name = this.name === 'Batman' ? 'John Doe' : 'Batman'
+      "
+    >
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button v-on:mouseenter="changeName" v-on:mouseout="changeName">
+      mouse enter OR mouse out to change Name
+    </button>
+  </p>
+
+  <hr />
+
+  <h2>{{ runningCounter }}</h2>
+  <p>
+    <button v-on:click="incrementCounter">increment Counter</button>
+    &nbsp;
+    <button v-on:click="resetCounter">reset Counter</button>
+    &nbsp;
+    <button v-on:click="decrementCounter">decrement Counter</button>
+  </p>
+  <p>
+    <button v-on:click="incrementationCounter(5)">
+      increment Counter by 5
+    </button>
+    &nbsp;
+    <button v-on:click="decrementationCounter(5)">
+      decrement Counter by 5
+    </button>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'John Doe',
+      runningCounter: 0,
+    }
+  },
+  methods: {
+    changeName() {
+      return (this.name = this.name === 'Batman' ? 'John Doe' : 'Batman')
+    },
+    incrementCounter() {
+      this.runningCounter++
+    },
+    incrementationCounter(number) {
+      this.runningCounter += number
+    },
+    resetCounter() {
+      this.runningCounter = 0
+    },
+    decrementCounter() {
+      this.runningCounter--
+    },
+    decrementationCounter(number) {
+      this.runningCounter -= number
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 18-event-handling-part-2/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <h2>{{ name }}</h2>
+  <p>
+    <button @click="changeName, incrementationCounter(2, $event)">
+      click to change Name
+    </button>
+  </p>
+  <p>
+    <button @mouseover="this.name = 'Daniel'">
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button @mouseover="name = 'Daniel'">
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button
+      @mouseover="this.name = this.name === 'Batman' ? 'John Doe' : 'Batman'"
+    >
+      mouseover to change Name
+    </button>
+  </p>
+  <p>
+    <button
+      @mouseenter="changeName, incrementCounter($event)"
+      @mouseout="changeName"
+    >
+      mouse enter OR mouse out to change Name
+    </button>
+  </p>
+
+  <hr />
+
+  <h2>{{ runningCounter }}</h2>
+  <p>
+    <button @click="incrementCounter">increment Counter</button>
+    &nbsp;
+    <button @click="resetCounter">reset Counter</button>
+    &nbsp;
+    <button @click="decrementCounter">decrement Counter</button>
+  </p>
+  <p>
+    <button @click="incrementationCounter(5, $event)">
+      increment Counter by 5
+    </button>
+    &nbsp;
+    <button @click="decrementationCounter(5)">
+      decrement Counter by 5
+    </button>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'John Doe',
+      runningCounter: 0,
+    }
+  },
+  methods: {
+    changeName(event) {
+      console.log('event = ', event)
+      return (this.name = this.name === 'Batman' ? 'John Doe' : 'Batman')
+    },
+    incrementCounter(event) {
+      this.runningCounter++
+      console.log('Event = ', event)
+    },
+    incrementationCounter(number, event) {
+      this.runningCounter += number
+      console.log('event = ', event)
+    },
+    resetCounter() {
+      this.runningCounter = 0
+    },
+    decrementCounter() {
+      this.runningCounter--
+    },
+    decrementationCounter(number) {
+      this.runningCounter -= number
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
