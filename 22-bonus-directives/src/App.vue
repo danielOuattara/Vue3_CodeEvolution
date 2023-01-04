38800309d162a38d4ae1e649bced89a0fdf259e3
danielOuattara <daniel.ouattara@gmx.com>
2023-01-04 23:54:32 +0100

    more on directives

---
+++ 22-bonus-directives/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2 v-once>{{ name }}</h2>
+  <button @click="name = `Batman`">Change Name Value</button>
+
+  <h2 v-pre>{{ name }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Daniel',
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
