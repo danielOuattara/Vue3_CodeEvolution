94158b0474626c89d2c032d5bf92bf6a442171ff
danielOuattara <daniel.ouattara@gmx.com>
2023-01-09 20:32:14 +0100

    component and props

---
+++ 30-component-props/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Greet name="Daniel" heroName=" Batman" />
+  <Greet name="John" heroName="Superman" />
+  <Greet name="Jana" heroName="Wonder woman" />
+  <Greet :name="this.name" :heroName="this.channel" />
+</template>
+
+<script>
+import Greet from "./components/Greet.vue";
+export default {
+  name: "App",
+  components: {
+    Greet,
+  },
+  data() {
+    return {
+      name: "Vishwas",
+      channel: "CodeEvolution",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ 30-component-props/src/components/Greet.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Hello {{ name }} - {{ heroName }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Greet",
+  props: ["name", "heroName"],
+};
+</script>
+
+<style></style>
