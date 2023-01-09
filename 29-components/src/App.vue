1fd71e9daf7247ec984216a6d81ecfb4f1b51636
danielOuattara <daniel.ouattara@gmx.com>
2023-01-09 20:31:28 +0100

    components in vue3

---
+++ 29-components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Greet />
+</template>
+
+<script>
+import Greet from "./components/Greet.vue";
+export default {
+  name: "App",
+  components: {
+    Greet,
+  },
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+</style>
---
+++ 29-components/src/components/Greet.vue
@@ -0,0 +1,9 @@
+<template><h2>Hello Daniel</h2></template>
+
+<script>
+export default {
+  name: "Greet",
+};
+</script>
+
+<style></style>
