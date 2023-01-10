12029deb790e130000ce0c16179c0e3492d390cd
danielOuattara <daniel.ouattara@gmx.com>
2023-01-10 22:12:36 +0100

    custom component events

---
+++ 34-custom-component-events/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2>Custom component events</h2>
+    <button @click="toggleShowPopup">
+      {{ !showPopup ? "show popup" : "hide popup" }}
+    </button>
+    <Popup
+      class="popup"
+      v-show="this.showPopup"
+      @close="this.closePopup"
+      @closePopup="this.closePopup"
+    />
+  </div>
+</template>
+
+<script>
+import Popup from "./components/Popup.vue";
+
+export default {
+  name: "App",
+  components: {
+    Popup,
+  },
+
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+
+  methods: {
+    toggleShowPopup() {
+      this.showPopup = !this.showPopup;
+    },
+    closePopup(arg) {
+      this.showPopup = false;
+      console.log("arg = ", arg);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+button {
+  border: 3px solid green;
+  padding: 10px;
+  border-radius: 7px;
+}
+.popup {
+  border: 1px solid grey;
+  padding: 20px;
+  width: 300px;
+  display: block;
+  margin: auto;
+  margin-top: 20px;
+}
+</style>
---
+++ 34-custom-component-events/src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>I am a pop up</h3>
+    <button @click="$emit('close', 'Daniel')">Close Popup</button>
+    <button @click="this.shutPopup">Close Popup</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Popup",
+  emits: ["close", "closePopup"],
+  methods: {
+    shutPopup() {
+      return this.$emit("closePopup", "Daniel");
+    },
+  },
+};
+</script>
