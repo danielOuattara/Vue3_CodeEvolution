2ba0a1f69903d7700f5c4fe73d073a242602dfab
danielOuattara <daniel.ouattara@gmx.com>
2023-01-10 22:13:11 +0100

    components and v-model

---
+++ 36-components-and-v-model/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- <label for="name"
+    >Enter your name:
+    <input type="text" name="name" id="name" v-model.lazy="this.name" />
+  </label> -->
+
+  <Input v-model="name" />
+</template>
+
+<script>
+import Input from "./components/Input.vue";
+export default {
+  name: "App",
+  components: {
+    Input,
+  },
+
+  data() {
+    return {
+      name: "",
+    };
+  },
+
+  methods: {
+    showName() {
+      console.log(this.name);
+    },
+  },
+
+  mounted() {
+    console.log("mounted: ", this.name);
+  },
+  updated() {
+    console.log("updated: ", this.name);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+button {
+  border: 3px solid green;
+  padding: 10px;
+  border-radius: 7px;
+}
+.popup {
+  border: 1px solid grey;
+  padding: 20px;
+  width: 300px;
+  display: block;
+  margin: auto;
+  margin-top: 20px;
+}
+</style>
---
+++ 36-components-and-v-model/src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="text"
+    name="name"
+    id="name"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    modelValue: String,
+  },
+};
+</script>
+
+<style>
+input[type="text"] {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
