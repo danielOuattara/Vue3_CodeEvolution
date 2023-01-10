784000a5a8409f98cf433d7439a519c9aaecd321
danielOuattara <daniel.ouattara@gmx.com>
2023-01-10 22:12:57 +0100

    validating emitted events

---
+++ 35-validating-emitted-events/src/components/Popup.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>I am a pop up</h3>
+    <div>
+      <label for="name"
+        >Enter your name:
+        <input type="text" name="name" id="name" v-model="name" />
+      </label>
+    </div>
+    <p>
+      <button @click="$emit('close', this.name)">Close Popup</button>
+      <button @click="this.shutPopup">Close Popup</button>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Popup",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  emits: {
+    close: (name) => {
+      if (!name) return false;
+      return true;
+    },
+    closePopup: (name) => {
+      if (!name) return false;
+      return true;
+    },
+  },
+  methods: {
+    shutPopup() {
+      return this.$emit("closePopup", this.name);
+    },
+  },
+};
+</script>
