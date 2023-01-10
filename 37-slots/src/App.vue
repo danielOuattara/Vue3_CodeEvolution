af7122c0e4287f3a9a43eb239d4f2b55f901843c
danielOuattara <daniel.ouattara@gmx.com>
2023-01-10 22:13:25 +0100

    slots in vue3

---
+++ 37-slots/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Card />
+  <Card>Card content</Card>
+  <Card>
+    <h2>Card content</h2>
+  </Card>
+  <Card> <img src="https://picsum.photos/200"/></Card>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+
+  data() {
+    return {
+      name: "",
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
