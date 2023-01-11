5455014a4ef2ac4d9735596dbd02bdfacdc087e1
danielOuattara <daniel.ouattara@gmx.com>
2023-01-11 23:24:09 +0100

    get request via axios

---
+++ 45-http-get-request/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>http get with axios</h1>
+  <PostList :posts="this.posts" />
+</template>
+
+<script>
+import axios from "axios";
+import PostList from "./components/PostList.vue";
+export default {
+  name: "App",
+  components: {
+    PostList,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          headers: {
+            "content-type": "application/json; charset=utf-8",
+          },
+          params: {
+            _limit: 30,
+          },
+        },
+      );
+      // console.log("response = ", response);
+      this.posts = response.data;
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
---
+++ 45-http-get-request/src/components/PostList.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="this.fetchData">Load posts</button>
+  <p v-if="this.fetchError">{{ this.fetchError }}</p>
+  <div v-for="post in this.listOfPosts" :key="post">
+    <p>{{ post.title }}</p>
+    <p>{{ post.id }}</p>
+  </div>
+  <hr />
+  <br />
+  <ul>
+    <li v-for="person in this.posts" :key="person.id">{{ person.title }}</li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["posts"],
+
+  data() {
+    return {
+      listOfPosts: [],
+      fetchError: "",
+    };
+  },
+
+  methods: {
+    fetchData() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/postsAAA")
+        .then((response) => (this.listOfPosts = response.data))
+        .catch((error) => {
+          this.fetchError = error.message;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
