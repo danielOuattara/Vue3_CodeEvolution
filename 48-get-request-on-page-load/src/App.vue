695ab96bdc020f846feaa6b87dd9f4eba8179216
danielOuattara <daniel.ouattara@gmx.com>
2023-01-12 22:08:29 +0100

    http request on loading

---
+++ 48-get-request-on-page-load/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>http get with axios</h1>
+  <ul>
+    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          params: { _limit: 30 },
+        },
+      );
+      console.log("response = ", response);
+      this.posts = response.data;
+    },
+  },
+  created() {
+    this.getData();
+  },
+  // mounted() {
+  //   this.getData();
+  // },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
