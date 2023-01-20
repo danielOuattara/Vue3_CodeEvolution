d3ebf8cedbeb65d05a1f9714896494e44ac1104b
danielOuattara <daniel.ouattara@gmx.com>
2023-01-20 23:15:00 +0100

    replacing data with ref function

---
+++ 52-replacing-data-with-ref-function/src/App.vue
@@ -0,0 +1,22 @@
+<template><Data /> </template>
+
+<script>
+import Data from "./components/Data.vue";
+export default {
+  name: "App",
+  components: { Data },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
---
+++ 52-replacing-data-with-ref-function/src/components/Data.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Options API - {{ o_firstName }}</h2>
+  <h2>Composition API - {{ c_firstName }}</h2>
+  <h2>Composition API - {{ greetings }}</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Data",
+  setup() {
+    const c_firstName = ref("Daniel");
+    console.log("c_firstName = ", c_firstName);
+
+    c_firstName.value = "Julie";
+    const greetings = `Hello ${c_firstName.value}`;
+    return { c_firstName, greetings };
+  },
+  data() {
+    return {
+      o_firstName: "Bruce",
+    };
+  },
+};
+</script>
