e623136d8a3b3a391a8ec9afd0b20977729c339c
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 12:34:08 +0100

    v-model and composition API

---
+++ 56-v-model-and-composition-api/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <VModel />
+</template>
+
+<script>
+import VModel from "./components/VModel.vue";
+
+export default {
+  name: "App",
+  components: {
+    VModel,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
---
+++ 56-v-model-and-composition-api/src/components/VModel.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Options API -->
+  <input type="text" placeholder="First name" v-model="fName" />
+  <input type="text" placeholder="Last name" v-model="lName" />
+
+  <hr />
+  <br />
+  <!-- Composition API -->
+  <input type="text" placeholder="Hero name" v-model="heroName" />
+  <input type="text" placeholder="First name" v-model="firstName" />
+  <input type="text" placeholder="Last name" v-model="lastName" />
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+
+export default {
+  name: "VModel",
+  setup() {
+    const heroName = ref("");
+    const state = reactive({ firstName: "", lastName: "" });
+    return { heroName, ...toRefs(state) };
+  },
+
+  data() {
+    return {
+      fName: "",
+      lName: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
