d14884ee42cdce99af22f168724d2244b881d18e
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 12:33:45 +0100

    composition API: replacing methods

---
+++ 55-replacing-methods/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Methods />
+</template>
+
+<script>
+// import Data from "./components/Data.vue";
+import Methods from "./components/Methods.vue";
+
+export default {
+  name: "App",
+  components: {
+    Methods,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
---
+++ 55-replacing-methods/src/components/Methods.vue
@@ -0,0 +1,67 @@
+<template>
+  <h2>{{ name }}</h2>
+  <button @click="setName">Change Name</button>
+
+  <hr />
+
+  <h2>Count: {{ c_count }}</h2>
+  <button @click="c_incrementCount">incrementCount</button>
+
+  <hr />
+
+  <h2>Count: {{ c_count }}</h2>
+  <button @click="c_incrementCount2">incrementCount_2</button>
+
+  <hr />
+
+  <h2>{{ firstName }} {{ lastName }}</h2>
+  <button @click="c_changeHero">Change user</button>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+export default {
+  name: "Methods",
+
+  setup() {
+    const c_count = ref(0);
+
+    const c_incrementCount = () => {
+      return c_count.value++;
+    };
+
+    function c_incrementCount2() {
+      return c_count.value++;
+    }
+
+    const user = reactive({
+      firstName: "Daniel",
+      lastName: "Ouattara",
+    });
+
+    function c_changeHero() {
+      user.firstName = "John";
+      user.lastName = "Travolta";
+    }
+
+    return {
+      c_count,
+      c_incrementCount,
+      c_incrementCount2,
+      ...toRefs(user),
+      c_changeHero,
+    };
+  },
+  data() {
+    return {
+      name: "Bruce",
+    };
+  },
+
+  methods: {
+    setName() {
+      this.name = "codeEvolution";
+    },
+  },
+};
+</script>
