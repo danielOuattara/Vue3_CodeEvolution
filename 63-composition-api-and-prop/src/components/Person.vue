4cd0dfae92fdf9bf694122d438403337c755fa43
danielOuattara <daniel.ouattara@gmx.com>
2023-01-24 23:06:45 +0100

    composition and props

---
+++ 63-composition-api-and-prop/src/components/Person.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" placeholder="First name" v-model="firstName" />
+    <input type="text" placeholder="Last name" v-model="lastName" />
+    <PersonGreeting :firstName="firstName" :lastName="lastName" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import PersonGreeting from "./PersonGreeting.vue";
+
+export default {
+  name: "Peron",
+  components: {
+    PersonGreeting,
+  },
+  setup() {
+    const firstName = ref("");
+    const lastName = ref("");
+    return { lastName, firstName };
+  },
+};
+</script>
---
+++ 63-composition-api-and-prop/src/components/PersonGreeting.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Person Greeting component</h2>
+    <p>basic props : {{ firstName }} {{ lastName }}</p>
+    <p>computed Options API : {{ fullName_option }}</p>
+    <p>computed Composition API : {{ fullName_composition }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "PersonGreeting",
+  props: ["firstName", "lastName"],
+  setup(props) {
+    const fullName_composition = computed(() => {
+      return `${props.firstName} ${props.lastName}`;
+    });
+    return { fullName_composition };
+  },
+
+  computed: {
+    fullName_option() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+};
+</script>
