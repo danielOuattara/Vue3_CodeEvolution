d115e62e679bb638b7280ff1da5c1b88c1ea2588
danielOuattara <daniel.ouattara@gmx.com>
2023-01-24 23:06:28 +0100

    template refs & composition

---
+++ 62-template-refs-and-composition-api/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <TemplateRef />
+</template>
+
+<script>
+import TemplateRef from "./components/TemplateRef.vue";
+
+export default {
+  name: "App",
+  components: {
+    TemplateRef,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  font-size: 24px;
+}
+</style>
---
+++ 62-template-refs-and-composition-api/src/components/TemplateRef.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input type="text" ref="inputRef" />
+    <input type="text" ref="c_inputRef" />
+    <button @click="c_handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  name: "TemplateRef",
+
+  setup() {
+    const c_inputRef = ref(null);
+    console.log("c_inpuRef = ", c_inputRef);
+    console.log("c_inputRef.value = ", c_inputRef.value);
+
+    function c_handleClick() {
+      console.log("c_inpuRef = ", c_inputRef);
+      console.log("c_inputRef.value = ", c_inputRef.value);
+    }
+
+    onMounted(function() {
+      console.log("c_inpuRef = ", c_inputRef);
+      console.log("c_inputRef.value = ", c_inputRef.value);
+      c_inputRef.value.focus();
+    });
+
+    return {
+      c_inputRef,
+      c_handleClick,
+    };
+  },
+
+  //-----------------------------------
+
+  // mounted() {
+  //   this.$refs.inputRef.focus();
+  // },
+};
+</script>
+
+<style lang="scss" scoped></style>
